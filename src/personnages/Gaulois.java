9fe36a404c1a8e2fcef565110412f1cc0144b216
Lucas <54537697+Luckman31@users.noreply.github.com>
2023-09-21 11:20:25 +0200

    En cours

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,55 @@
+package personnages;
+
+import java.security.PublicKey;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class Gaulois {
+	private String nomString;
+	@Override
+	public String toString() {
+		return "Gaulois [nomString=" + nomString + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	private int force;
+	private int effetPotion = 1;
+	public Gaulois(String nom, int force) {
+		super();
+		this.nomS = nom;
+		this.force = force;
+	}
+public static void main(String[] args) {
+	if (condition) {
+		
+	}
+	switch (key) {
+	case value:
+		
+		break;
+
+	default:
+		break;
+	}
+	for (int i = 0; i < args.length; i++) {
+		
+	}
+	do {
+		
+	} while (condition);
+	public void parler() {
+		prendreParole;
+		
+		
+		
+		
+		
+		
+	}
+}
+private static void prendreParole() {
+	// TODO Auto-generated method stub
+	
+}
+
+}
+
+
