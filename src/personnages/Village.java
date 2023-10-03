a5fa00a88658283e205d6fd1bed198fd0639d68c
Lucas <54537697+Luckman31@users.noreply.github.com>
2023-10-03 09:26:04 +0200

    En cours

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -25,7 +25,23 @@ public class Village{
 		return nom;
 	
 	}
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef "+chef.getNom()+" vivent les légendaires gaulois :");
+		for(int i=0;i!=nbVillageois;i++) {
+			System.out.println("-"+villageois[i].getNom());
+		}
+		
+	}
 	public static void main(String[] args) {
 		Village village = new Village("Village des Irréductibles", 30);
+//		Gaulois gaulois = village.trouverHabitant(30);
+		Chef abraracourix=new Chef("Abraracourix",6,village);
+		Gaulois asterix=new Gaulois("Asterix",8);
+//		Gaulois gaulois = village.trouverHabitant(1);
+//		System.out.println(gaulois);
+		Gaulois obelix=new Gaulois("Obelix",25);
+		village.ajouterHabitant(obelix);
+		village.setChef(abraracourix);
+		village.afficherVillageois();
 	}
 }
